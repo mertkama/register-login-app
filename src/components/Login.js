6c613b6ab2f254d889f389688dba396701816a1d
DESKTOP-VHLAITO\mert1 <mert172317@gmail.com>
2022-07-02 13:08:10 +0300

    input update

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,15 @@ const Login = () => {
             <form className="login__form">
                 <h2 className="login__title">Create Your Account </h2>
                 <h4 className="login__subtitle">Created for developers by developers</h4>
-                input
+                <input type="text" name="username" id="username" />
+                <input type="email" name="email" id="email" />
+                <input type="password" name="password" id="password" />
+                <div>
+                    <input type="checkbox" name="checkbox" id="checkbox" />
+                    <span>
+                    I agree to the Terms & Conditions
+                    </span>
+                </div>
             </form>
         </div>
     );
